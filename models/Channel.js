e5e0212efe792af91f80af32b1c1d47acc80ab88
Mithat Can <84298586+mithatcy@users.noreply.github.com>
2023-04-01 02:38:46 +0300

    Delete models directory

--- models/Channel.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const ChannelSchema = new Schema({
-    guildID: String,
-    channels: Array
-});
-
-const ChannelModel = model("channelbackup", ChannelSchema);
-module.exports = ChannelModel;
--- models/deletedRoles.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const data = mongoose.Schema({
-    oldRoleID: String,
-    newRoleID: String,
-    Date: Number
-});
-
-module.exports = mongoose.model("deletedRole", data);
